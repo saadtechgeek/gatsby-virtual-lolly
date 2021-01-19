bbcaf1fce77e3f60c9742f3fa1f10b0ca1ae735e
saadtechgeek <saad.techgeek@gmail.com>
2021-01-19 12:01:32 +0500

    showed lolly and added readme file

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-
+import Lolly from './../components/lolly'
 const Home = () => {
     return (
-        <div>Hello World</div>
+        <div>
+            <Lolly/>
+
+        </div>
     )
 }
 export default Home;
