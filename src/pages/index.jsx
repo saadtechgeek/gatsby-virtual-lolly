cb31937d2a9e0dd6a6f3d690e55c866dedabb57a
saadtechgeek <saad.techgeek@gmail.com>
2021-01-19 12:32:03 +0500

    fixed header issue

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,14 @@
 import React, {useState} from 'react';
 import Lolly from './../components/lolly'
+import "./styles.css";
+
 const Home = () => {
     const [c1, setC1] = useState('#e95946');
     const [c2, setC2] = useState('#d52358');
     const [c3, setC3] = useState('#deaa43');
     return (
+        <div className="container">
+        <h1>Create Lolly</h1>
         <div className="main-container">
             <div>
                 <Lolly top={c1} middle={c2} bottom={c3} />
@@ -12,6 +16,14 @@ const Home = () => {
                 <input type="color" value={c2} onChange={(e) => { setC2(e.target.value) }} />
                 <input type="color" value={c3} onChange={(e) => { setC3(e.target.value) }} />
             </div>
+            <div className="form-container">
+                <input type="text" placeholder="To" />
+                <textarea  placeholder="Enter your message!" />
+                <input type="text" placeholder="From" />
+
+                <button>Send</button>
+            </div>
+        </div>
         </div>
     )
 }
