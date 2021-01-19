ab9f920b65ae774fac38c3f0ef2c115675ecea09
saadtechgeek <saad.techgeek@gmail.com>
2021-01-19 13:03:39 +0500

    form submit

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState,useRef} from 'react';
 import Lolly from './../components/lolly'
 import "./styles.css";
 
@@ -6,6 +6,27 @@ const Home = () => {
     const [c1, setC1] = useState('#e95946');
     const [c2, setC2] = useState('#d52358');
     const [c3, setC3] = useState('#deaa43');
+
+    const handleSubmit = () => {
+        console.log(senderField.current.value)
+        console.log(recField.current.value)
+        console.log(msgField.current.value)
+        // addVCard({
+        //     variables: {
+        //         c1, c2, c3,
+        //         rec: recField.current.value,
+        //         sender: senderField.current.value,
+        //         msg: msgField.current.value
+        //     }
+        // })
+    }
+
+    const senderField = useRef();
+    const recField = useRef();
+    const msgField = useRef();
+    
+
+
     return (
         <div className="container">
         <h1>Create Lolly</h1>
@@ -17,11 +38,10 @@ const Home = () => {
                 <input type="color" value={c3} onChange={(e) => { setC3(e.target.value) }} />
             </div>
             <div className="form-container">
-                <input type="text" placeholder="To" />
-                <textarea  placeholder="Enter your message!" />
-                <input type="text" placeholder="From" />
-
-                <button>Send</button>
+                <input type="text" placeholder="To" ref={recField} />
+                <textarea placeholder="Enter your message!" ref={msgField}></textarea>
+                <input type="text" placeholder="From" ref={senderField} />
+                <button onClick={handleSubmit}>Send</button>
             </div>
         </div>
         </div>
